1b58f2dd7d93e147195ec55c5eb138a2a6dbc645
minhhn3 <minhhn3@vng.com.vn>
2022-02-28 23:13:36 +0700

    feat: update go mod

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/minhhoangvn/testkube-executor-k6
 go 1.16
 
 require (
-	github.com/bclicn/color v0.0.0-20180711051946-108f2023dc84 // indirect
 	github.com/kubeshop/testkube v0.8.5
 	go.uber.org/zap v1.20.0
+	k8s.io/apimachinery v0.23.4
+	k8s.io/client-go v0.23.4
 )
--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -1,6 +1,15 @@
 package runner
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 	"github.com/kubeshop/testkube/pkg/executor"
 	"github.com/kubeshop/testkube/pkg/log"
@@ -17,6 +26,7 @@ type K6Runner struct {
 }
 
 func (r *K6Runner) Run(execution testkube.Execution) (result testkube.ExecutionResult, err error) {
+	r.getClusterConfig()
 	output, err := executor.Run("", "k6", "--help")
 	if err != nil {
 		r.Log.Errorf("Error occured when running a command %s", err)
@@ -28,4 +38,49 @@ func (r *K6Runner) Run(execution testkube.Execution) (result testkube.ExecutionR
 		Status: testkube.StatusPtr(testkube.SUCCESS_ExecutionStatus),
 		Output: outputString,
 	}, nil
+}
+
+func (r *K6Runner) getClusterConfig() {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(config.String())
+	fmt.Printf("%+v\n", config)
+	countPrint := 1
+	for countPrint < 5 {
+		countPrint++
+		// get pods in all the namespaces by omitting namespace
+		// Or specify namespace to get pods in particular namespace
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		for idx, pod := range pods.Items {
+			fmt.Println(idx, pod.Name)
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+		// Examples for error handling:
+		// - Use helper functions e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		_, err = clientset.CoreV1().Pods("testkube").Get(context.TODO(), "testkube-api-server", metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod testkube-api-server not found in default namespace\n")
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found testkube-api-server pod in default namespace\n")
+		}
+
+		time.Sleep(10 * time.Second)
+	}
 }
